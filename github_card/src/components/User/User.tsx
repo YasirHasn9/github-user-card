9f31debe356121de4bc5e5a3ba519b984e448f6b
Yasir Hassan <hasanyasir01@gmail.com>
2021-01-31 09:50:42 -0800

    passed props contains user to User Components

---
+++ github_card/src/components/User/User.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const User = (props) => {
+    console.log("props",props)
+    return (
+        <div>
+            User
+        </div>
+    )
+}
+
+export default User
